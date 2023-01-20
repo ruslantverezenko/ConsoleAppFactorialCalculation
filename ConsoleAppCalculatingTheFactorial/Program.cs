f8b9e02ccb1320f5754b9b58a2dc4bff3a5b497f
Ruslan Tverezenko <68191748+ruslantverezenko@users.noreply.github.com>
2023-01-21 00:52:15 +0300

    factorial calculation
    Task: ask the user for a number whose factorial needs to be calculated and calculated. Then output the result of the calculation. There is no need to request an exclamation mark, in addition, there is no such operation in C#. To calculate the factorial, it is necessary to multiply.

---
+++ ConsoleAppCalculatingTheFactorial/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculatingTheFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                CalculatingTheFactorial();
+                
+                Console.Write("Again?(yes or no): ");
+                string userSelection = Console.ReadLine();
+                if (userSelection == "no")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            
+
+        }
+        static void CalculatingTheFactorial()
+        {
+            Console.Write("Enter a number to calculate the factorial(without !): ");
+            int num = int.Parse(Console.ReadLine());
+            int faktorial = 1;
+            for (int i = 1; i < num + 1; i++)
+            {
+                faktorial = faktorial * i;
+            }
+            Console.WriteLine($"factorial: {faktorial}");
+        }
+    }
+}
